5bdbc1136ea265b09db56d7dbfc5a4f1c3bc9c17
pigo271b <pigo271b@tauruslogin3.taurus.hrsk.tu-dresden.de>
2022-04-11 12:32:30 +0200

    from cluster

--- experiments/rule_dd/rule_dd_constants.py
+++ experiments/rule_dd/rule_dd_constants.py
@@ -1,4 +1,4 @@
-JAR_LOCATION = 'C:\\Projects\\aba-experiments\\experiments\\rule_dd\\runner.jar'
+JAR_LOCATION ="runner.jar" 
 JAR_SCRIPT = f'-cp {JAR_LOCATION} experiments.runner.actualFinalExperiments.Runner'
 
-OUTPUT_MAIN_DIR = 'C:\\Projects\\aba-experiments\\experiments\\outputs'
+OUTPUT_MAIN_DIR="outputs"
