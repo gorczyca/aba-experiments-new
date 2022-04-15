0e8eee8324a2c1cf5a413719997c35f2a5106734
gorczyca <pigo271b@tauruslogin4.taurus.hrsk.tu-dresden.de>
2022-04-15 17:39:31 +0200

    corrected paths in runners

--- experiments/rule_dd/scripts/adm_approx/runner.py
+++ experiments/rule_dd/scripts/adm_approx/runner.py
@@ -2,7 +2,7 @@ import pathlib
 import subprocess
 
 if __name__ == '__main__':
-    files = list(pathlib.Path('..').rglob(f"*.sh"))
+    files = list(pathlib.Path('.').rglob(f"*.sh"))
 
     for file in files:
         paramenters = ['nohup', 'sbatch', file, '&']
