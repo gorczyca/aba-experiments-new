a25786a8323e5a9fb3a2dcb05e8396c77b3b0a5c
gorczyca <gorczycapj@gmail.com>
2022-05-06 14:54:34 +0200

    runner

--- experiments/rule_dd/scripts/runner.py
+++ experiments/rule_dd/scripts/runner.py
@@ -1,11 +1,14 @@
 import pathlib
 import subprocess
+import sys
 
 if __name__ == '__main__':
-    files = list(pathlib.Path('adm_approx').rglob(f"*.sh"))
+
+    scripts_path = sys.argv[1]
+    files = list(pathlib.Path(scripts_path).rglob(f"*.sh"))
 
     for file in files:
         paramenters = ['nohup', 'sbatch', file, '&']
         output = subprocess.check_output(paramenters, stderr=subprocess.STDOUT)
-        print('jobs submitted!')
+        print('job submitted!')
 
