01acc1d20b52d01afaa83bd765f3a27bfdcff3c5
gorczyca <gorczycapj@gmail.com>
2022-05-06 15:34:47 +0200

    ehhh,,,

--- experiments/rule_dd/rule_dd_constants.py
+++ experiments/rule_dd/rule_dd_constants.py
@@ -1,4 +1,4 @@
-JAR_LOCATION ="runner.jar" 
+JAR_LOCATION ="/beegfs/ws/0/pigo271b-aba_ws_3/experiments2/aba-experiments-new/experiments/rule_dd/runner.jar"
 JAR_SCRIPT = f'-cp {JAR_LOCATION} experiments.runner.actualFinalExperiments.Runner'
 
-OUTPUT_MAIN_DIR="outputs"
+OUTPUT_MAIN_DIR = "outputs"
