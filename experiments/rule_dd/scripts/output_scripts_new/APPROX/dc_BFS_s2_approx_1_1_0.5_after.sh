00d696b5dfefcaea0cd47ce8f904caf1f96e279a
gorczyca <gorczycapj@gmail.com>
2022-05-06 23:07:59 +0200

    approx (hopefully)

---
+++ experiments/rule_dd/scripts/output_scripts_new/APPROX/dc_BFS_s2_approx_1_1_0.5_after.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=100:00:00
+#SBATCH --mem-per-cpu=16384
+#SBATCH --job-name=2_dc_BFS_s2_approx_1_1_0.5_after
+#SBATCH --mail-type=end
+#SBATCH --mail-user=pigo271b@tu-dresden.de
+
+PYTHON_SCRIPT_LOC="/beegfs/ws/0/pigo271b-aba_ws_3/experiments2/aba-experiments-new/experiments/rule_dd/run_ruledd_from_csv.py"
+
+# the next two have to be relative to the python script!
+CSV_LOC="/beegfs/ws/0/pigo271b-aba_ws_3/experiments2/aba-experiments-new/experiments/all_goals.csv"
+#CSV_LOC="../all_goals.csv"
+INSTANCES_DIR_LOC="/beegfs/ws/0/pigo271b-aba_ws_3/experiments2/aba-experiments-new/instances/rule_dd_instances"
+#INSTANCES_DIR_LOC="../../instances/rule_dd_instances"
+EXPERIMENT_NAME="2_dc_BFS_s2_approx_1_1_0.5_after_exp"
+TIMEOUT=600
+
+srun --exclusive --ntasks=1 python $PYTHON_SCRIPT_LOC $CSV_LOC $INSTANCES_DIR_LOC $EXPERIMENT_NAME $TIMEOUT -i apx --mode approx --one 1 --dfs 0 --tc TA --da dc --swa 0 --ordering pf1ob1ob2of2pb1pb2pf2 --prc smin --orc smin --phc l --ohc l --propP 1 --oppP 0.5 --sampleBefore 0
+
+
+echo "waiting for all jobs to complete"
+wait
+echo "all parallel tasks finished"
+
+
---
+++ experiments/rule_dd/scripts/output_scripts_new/APPROX/ds_DFS_s3a_approx_1_0.6_1_before.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=100:00:00
+#SBATCH --mem-per-cpu=16384
+#SBATCH --job-name=2_ds_DFS_s3a_approx_1_0.6_1_before
+#SBATCH --mail-type=end
+#SBATCH --mail-user=pigo271b@tu-dresden.de
+
+PYTHON_SCRIPT_LOC="/beegfs/ws/0/pigo271b-aba_ws_3/experiments2/aba-experiments-new/experiments/rule_dd/run_ruledd_from_csv.py"
+
+# the next two have to be relative to the python script!
+CSV_LOC="/beegfs/ws/0/pigo271b-aba_ws_3/experiments2/aba-experiments-new/experiments/all_goals.csv"
+#CSV_LOC="../all_goals.csv"
+INSTANCES_DIR_LOC="/beegfs/ws/0/pigo271b-aba_ws_3/experiments2/aba-experiments-new/instances/rule_dd_instances"
+#INSTANCES_DIR_LOC="../../instances/rule_dd_instances"
+EXPERIMENT_NAME="2_ds_DFS_s3a_approx_1_0.6_1_before_exp"
+TIMEOUT=600
+
+srun --exclusive --ntasks=1 python $PYTHON_SCRIPT_LOC $CSV_LOC $INSTANCES_DIR_LOC $EXPERIMENT_NAME $TIMEOUT -i apx --mode approx --one 1 --dfs 1 --tc TA --da ds --swa 0 --ordering pf1pb2pf2pb1ob2of2ob1 --prc smax --orc smax --phc m --ohc m --propP 0.6 --oppP 1 --sampleBefore 1
+
+
+echo "waiting for all jobs to complete"
+wait
+echo "all parallel tasks finished"
+
+
